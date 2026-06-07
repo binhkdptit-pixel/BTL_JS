7bf438c52818a26c78ccdc4de4edfdf4e5947179
BachIO <dnbach.ptit@gmail.com>
2026-06-07 19:18:28 +0700

    Thêm JS, thêm thanh tìm kiếm

--- BTL_JS-master/js/main.js
+++ BTL_JS-master/js/main.js
@@ -1,11 +1,26 @@
 function updateCartCount() {
-    const cart = JSON.parse(localStorage.getItem("cart")) || [];
-    let count = 0;
-    cart.forEach(item => count += item.quantity);
-    document.getElementById("cartCount").textContent = count;
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let count = 0;
+  cart.forEach((item) => (count += item.quantity));
+  document.getElementById("cartCount").textContent = count;
 }
 
 function formatPrice(price) {
-    return price.toLocaleString("vi-VN") + " VNĐ";
-    
+  return price.toLocaleString("vi-VN") + " VNĐ";
 }
+
+//Thanh tìm kiếm
+document.getElementById("search-input").addEventListener("input", function (e) {
+  let từKhóa = e.target.value.toLowerCase();
+  let cácMónĂn = document.querySelectorAll(".product-card");
+
+  cácMónĂn.forEach((món) => {
+    let tênMón = món.querySelector("h4").textContent.toLowerCase();
+
+    if (tênMón.includes(từKhóa)) {
+      món.style.display = "block";
+    } else {
+      món.style.display = "none";
+    }
+  });
+});
